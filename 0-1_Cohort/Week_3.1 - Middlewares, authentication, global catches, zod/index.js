e86c75c1349f01101c76ffec4f916fc5d0a0fe0a
anasnadkar45 <anasnadkar23@gmial.com>
2023-12-23 10:56:07 +0530

    Added code files

--- 0-1_Cohort/Week_3.1 - Middlewares, authentication, global catches, zod/index.js
+++ 0-1_Cohort/Week_3.1 - Middlewares, authentication, global catches, zod/index.js
@@ -54,28 +54,107 @@
 
 // ------------------------------------------------------------------------------------------------
 
-// Input validation:-
+//Zod Input validation:-
 
 const express = require('express');
 const app = express();
+const zod = require('zod');
+
+const schema = zod.array(zod.number());
 
 app.use(express.json());
 
 app.post('/health-checkup', function (req, res) {
     const kidneys = req.body.kidneys;
-    const kidneysLength = kidneys.length;
-
-    res.send(`Length of an kidney: ${kidneysLength}`);
-
+    // const kidneysLength = kidneys.length;
+    // if (!kidneys || !Array.isArray(kidneys)) {
+    //     res.status(400).send({ error: 'Invalid or missing kidneys property in the request body.' });
+    //     return;
+    // }
+    const response = schema.safeParse(kidneys);
+    // res.send({
+    //     response
+    // })
+    // res.send(`Length of an kidney: ${kidneysLength}`);
+    if(!response.success){
+        res.status(411).json({
+            msg:"input is invalid"
+        })
+    }else{
+        res.send({
+            response
+        })
+    }
 })
 
-app.use((error, req, res, next) => {
-    res.status(500).send("An error occurred")
-})
+// app.use((error, req, res, next) => {
+//     res.status(500).send("An error occurred")
+// })
 
 app.listen(3000, () => {
     console.log('server listening on port 3000');
 })
+
+
+// ------------------------------------------------------------------------------------------------
+// schema for input validation in the context of social media
+
+
+// const { z } = require('zod');
+
+// const userProfileSchema = z.object({
+//     username: z.string().min(3).max(20),
+//     fullName: z.string().min(1).max(50),
+//     bio: z.string().max(200),
+//     email: z.string().email(),
+//     avatarUrl: z.string().url(),
+//     posts: z.array(z.object({
+//         postId: z.string(),
+//         content: z.string().max(280),
+//         createdAt: z.date(),
+//         likes: z.array(z.string()), // Assuming user IDs for likes
+//         comments: z.array(z.object({
+//             commentId: z.string(),
+//             userId: z.string(),
+//             content: z.string().max(140),
+//             createdAt: z.date(),
+//         })),
+//     })),
+// });
+
+// // Example usage:
+// const validProfileData = {
+//     username: 'john_doe',
+//     fullName: 'John Doe',
+//     bio: 'Software Developer',
+//     email: 'john@example.com',
+//     avatarUrl: 'https://example.com/avatar.jpg',
+//     posts: [
+//         {
+//             postId: 'post1',
+//             content: 'This is a post!',
+//             createdAt: new Date(),
+//             likes: ['user1', 'user2'],
+//             comments: [
+//                 {
+//                     commentId: 'comment1',
+//                     userId: 'user3',
+//                     content: 'Nice post!',
+//                     createdAt: new Date(),
+//                 },
+//             ],
+//         },
+//     ],
+// };
+
+// const validationResult = userProfileSchema.safeParse(validProfileData);
+
+// if (validationResult.success) {
+//     console.log('Validation successful:', validationResult.data);
+// } else {
+//     console.error('Validation failed:', validationResult.error.errors);
+// }
+
 //------------------------------------------------------------------------------------------------
 //Assignments : 1
 // const express = require("express");
