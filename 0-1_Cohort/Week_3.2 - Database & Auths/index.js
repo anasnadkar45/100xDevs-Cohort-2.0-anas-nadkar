37a15114e0645a4d464066c6c3229237bb704ef4
anasnadkar45 <anasnadkar23@gmial.com>
2023-12-24 16:39:28 +0530

    Added code files

---
+++ 0-1_Cohort/Week_3.2 - Database & Auths/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+// add json middleware to parse json requests
+app.use(express.json());
+
+// create a user object
+const USERS =[
+    {
+        username: 'anas',
+        password: 'nadkar-anas45',
+    },
+    {
+        username: 'vedant',
+        password: '4crore-chaiye!!',
+    },
+    {
+        username: 'shadab',
+        password: 'gyan-matDeChal',
+    },
+];
+
+app.post('/login', (req, res) => {
+    // get username and password from request body
+    const {username, password} = req.body;
+
+    //check if user exists
+    const user = USERS.find(user => user.username === username)
+
+    // if user does not exist
+    if(!user){
+        return res.status(400).json({
+            message:"user does not exist"
+        })
+    }
+
+    if(user.password !== password){
+        return res.status(400).json({
+            message:"password is incorrect"
+        })
+    }
+
+    res.json({message:"login success"})
+})
+
+app.listen(3000);
