bf3daf1a6a48e9905b3800fb8622ad9bb1893ca8
anasnadkar45 <anasnadkar23@gmial.com>
2024-01-01 11:23:11 +0530

    Adding uncommited files

---
+++ 0-1_Cohort/Week_4.4 - Mongo with Authentication/db/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://anasnadkar23:Anasnadkar45%40@cluster0.wo2o0ul.mongodb.net/course_selling_app2');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
